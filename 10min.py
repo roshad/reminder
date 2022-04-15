f1c45acd91f5d15a38d94a4c00a7ec83edfdfba5
roshad <roshad87a9@gmail.com>
2022-04-15 21:16:41 +0800

    准备回退

--- 10min.py
+++ 10min.py
@@ -3,22 +3,24 @@ import time
 import datetime
 import pygetwindow as gw
 import os
-from win10toast import ToastNotifier
+from win10toast_click import ToastNotifier 
 
 startTime = datetime.datetime.now()
 toast = ToastNotifier()
 # 一直开着对起床以后的大段浪费效果会好点
-# 讲不长,不如不讲
+
 while True:
     try:
         
         if ((datetime.datetime.now()-startTime).seconds >= 600):  # 到10分钟,放声音,激活
             print("弹窗", datetime.datetime.now().strftime('%H:%M:%S'))
-
+            
+            
+            
             toast.show_toast(
                 "多离 多息",
-                duration=10,
-                threaded=True,  # 否则程序中止
+                duration=None,
+                threaded=False,  # 否则程序中止
             )
             startTime=datetime.datetime.now()
             print("开始计时", startTime.strftime('%H:%M:%S'))
