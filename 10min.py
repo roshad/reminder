89fac2b2162ad83935f7e9717ebfe258fa14959d
roshad <roshad87a9@gmail.com>
2022-04-16 19:38:17 +0800

    0.1s刷新

--- 10min.py
+++ 10min.py
@@ -1,31 +1,34 @@
 
+from playsound import playsound
 import time
 import datetime
-import pygetwindow as gw
+import pygetwindow as gw  
 import os
-from win10toast_click import ToastNotifier 
 
-startTime = datetime.datetime.now()
-toast = ToastNotifier()
-# 一直开着对起床以后的大段浪费效果会好点
+startTime=None
+# 10min响一次,开terminal重置,偶发无意识地重置
 
 while True:
     try:
+        #print(gw.getActiveWindowTitle())
+        if ("OneNote" in gw.getActiveWindowTitle()):#在前 时间到了,在前,再重开,否则经常
+            if (startTime):#正计时,停止计时
+                startTime=None
+                print("停止计时")
+        else: #在背
+            if (startTime==None): #开始计时
+                startTime=datetime.datetime.now()
+                print("开始计时",startTime)
+            if ((datetime.datetime.now()-startTime).seconds>=600):#到10分钟,放声音,激活
+                print("时间到",datetime.datetime.now().strftime('%H:%M:%S'))
+                playsound("min.mp3")
+                #不能时间到就激活,否则无意中点旁边
         
-        if ((datetime.datetime.now()-startTime).seconds >= 600):  # 到10分钟,放声音,激活
-            print("弹窗", datetime.datetime.now().strftime('%H:%M:%S'))
-            
-            
-            
-            toast.show_toast(
-                "多离 多息",
-                duration=None,
-                threaded=False,  # 否则程序中止
-            )
-            startTime=datetime.datetime.now()
-            print("开始计时", startTime.strftime('%H:%M:%S'))
-
-            
-    except Exception as e:
+    except Exception as e: 
         print(e)
     time.sleep(0.1)
+
+
+
+
+
---
+++ playsound_test.py
@@ -0,0 +1,7 @@
+import os
+from playsound import playsound
+
+
+a= os.path.dirname(__file__)+'\\min.mp3'
+
+playsound("min.mp3")
