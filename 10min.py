c265adf7b6a800b5d1007df77ac886cdd8ae7e82
roshad <roshad87a9@gmail.com>
2022-05-12 12:07:20 +0800

    10min1h

--- 10min.py
+++ 10min.py
@@ -5,28 +5,27 @@ import datetime
 import pygetwindow as gw  
 import os
 
-startTime=None
+lastTime=datetime.datetime.now()
 # 10min响一次,开terminal重置,偶发无意识地重置
 a= os.path.dirname(__file__)+'\\min.wav'
 playsound(a)
 while True:
     try:
-        #get current hour
-        
-        if (startTime==None): #未计时,开始计时
-            startTime=datetime.datetime.now()
-            print("开始计时",startTime)
-        elif ((datetime.datetime.now()-startTime).seconds>=600 ):#时间到
-            if ("OneNote" not in gw.getActiveWindowTitle()):#没有在记录
-                print("鸣",datetime.datetime.now().strftime('%H:%M:%S'))
-                playsound(a)
-                #不能时间到就激活,否则无意中点旁边
-            else:   
-                startTime=None
-                print("停止计时")
+       
+        if (datetime.datetime.now().hour!=lastTime.hour):
+            playsound(a) 
+            playsound(a)
+            lastTime=datetime.datetime.now()
+            print("1小时")
+        elif ((datetime.datetime.now().minute-lastTime.minute)>=10 ):#时间到
+          
+            playsound(a)
+            #不能时间到就激活,否则无意中点旁边
+            lastTime=datetime.datetime.now()
+            print("10分钟")
     except Exception as e: 
         print(e)
-    time.sleep(0.1)
+    time.sleep(1)
 
 
 
