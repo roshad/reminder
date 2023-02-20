ed659f047d5cbe455d7535f4fcb0e48c13800061
roshad <roshad87a9@gmail.com>
2023-02-20 08:18:12 +0800

    借助browser

--- 10min.py
+++ 10min.py
@@ -4,29 +4,26 @@ import datetime
 import pygetwindow as gw  
 
 
-startTime=None
+startTime=datetime.datetime.now()
 # 10min响一次,开terminal重置,偶发无意识地重置
 
 while True:
     try:
         #get current hour
         
-        if (startTime==None): #未计时,开始计时
-            startTime=datetime.datetime.now()
-            print("开始计时",startTime)
-        elif ((datetime.datetime.now()-startTime).seconds>=600 ):#时间到
-            if ("OneNote" not in gw.getActiveWindowTitle()):#没有在记录
+       
+        if ((datetime.datetime.now()-startTime).seconds%300==0 ):#时间到           
+            if ("Track" not in gw.getActiveWindowTitle()):
                 print("提醒",datetime.datetime.now().strftime('%H:%M:%S'))
                 # activate OneNote window
-                gw.getWindowsWithTitle('OneNote')[0].activate()
-                
+                gw.getWindowsWithTitle('Toggl')[0].minimize()
+                gw.getWindowsWithTitle('Toggl')[0].maximize()
+                gw.getWindowsWithTitle('Toggl')[0].activate()
                 #不能时间到就激活,否则无意中点旁边
-            else:   
-                startTime=None
-                print("停止计时")
+        
     except Exception as e: 
         print(e)
-    time.sleep(0.1)
+    time.sleep(0.5)
 
 
 
