f1421d28ebfdaa7520441f8e4bfface4392df037
roshad <roshad87a9@gmail.com>
2023-06-03 17:26:04 +0800

    经常报错无法处理

--- 周期询问.py
+++ 周期询问.py
@@ -9,19 +9,26 @@ root = tk.Tk()
 # 检查是否有对应标题的窗口，有，等10分
 def check_window(title,dialog):
     # print(title in gw.getAllTitles())
-    dialog.destroy()
+    while True:
+        try:
+            dialog.destroy()
+            break
+        except:
+            print("Something else went wrong")
+    
     if (title == ""):
         # print(1)
         get_input()
     elif any(title in t for t in gw.getAllTitles()):
         print(datetime.datetime.now(),title)
-        time.sleep(10*60)
-        get_input()
+        
+        root.after(60*1000*10,lambda:get_input())
     else:
         # print(3)
         get_input()
     
 def get_input():
+    
     # create a new window for the input dialog
     dialog = tk.Toplevel(root)
 
@@ -34,19 +41,27 @@ def get_input():
     entry = tk.Entry(dialog)
     
     entry.bind("<Return>",  lambda event:check_window(entry.get(),dialog))
-    entry.after(60000,lambda :check_window(entry.get(),dialog))
-    # gain focus 
-    entry.focus_set()
-    dialog.state("zoomed")
+    
+    
         
+    entry.after(60*1000,lambda :check_window(entry.get(),dialog))
+    # gain focus tkinter
+    
+    
+    dialog.state("zoomed")
+    
+    
     # add the label, the text field, and the button to the window
     label.pack()
+    
     entry.pack()
     
-
+    
     # raise the window to the top of the stacking order and make it stay on top
     dialog.lift()
     dialog.attributes("-topmost", True)
+    
+    entry.focus_set()   
 # define a function that is called when the focus is lost
 
 # create a button that prompts the user for input
